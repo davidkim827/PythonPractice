d2fd30f486dd20757eade5b5510dd350f800eb11
davidkim827 <davidkim827@gmail.com>
2018-07-10 17:36:42 -0400

    Binary Search Guessing Game

---
+++ PracticePython Exercises/25-GuessingGameTwo.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+def bSearchGuess(listInput):
+    
+    L = 0
+    R = len(listInput)
+    
+    mid = (L + R) // 2
+    print(f"Is it {mid}, y or n?")
+    userBool = input()
+    if userBool == 'y':
+        return
+    else:
+        while userBool != 'r':
+            print(f"Is it higher (h) or lower (l) or am I right (r)?")
+            userBool = input()
+            if userBool == "l":
+                R = mid - 1
+            elif userBool == "h":
+                L = mid + 1
+            elif userBool == "r":
+                return
+            else:
+                print(f"That's not a valid response")
+                print(f"Is it higher (h) or lower (l) or am I right (r)?")
+                userBool = input()
+
+            mid = (L + R) // 2
+            print(f"{mid}")
+
+    return False
+
+
+numList = [i for i in range(0,101)]     #creates a list of numbers 0 to 100
+print("Please think of a number that the computer will have to guess between 0 and 100")
+print("\nIt\'s time to guess your number! Please follow the prompts") 
+
+bSearchGuess(numList)
+
+print("I guessed the right answer! Game is now over.")
+
+
