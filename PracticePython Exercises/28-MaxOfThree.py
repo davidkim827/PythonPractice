f7f90bd048865083c8bac3078b71db36f7e32218
davidkim827 <davidkim827@gmail.com>
2018-07-12 02:48:39 -0400

    Ex 28

---
+++ PracticePython Exercises/28-MaxOfThree.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def maxNum(val1, val2, val3):
+        
+    tmp = -1000000
+    if val1 > tmp:
+        tmp = val1
+    if val2 > tmp:
+        tmp = val2
+    if val3 > tmp:
+        tmp = val3
+    
+    return tmp
+
+print(maxNum(100,-6,-5))
+    
