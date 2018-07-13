12ebd7af275808b8982739a4ddb0735a08c8224a
davidkim827 <davidkim827@gmail.com>
2018-07-13 17:45:45 -0400

    Made it a function

--- PracticePython Exercises/24-DrawAGameBoard.py
+++ PracticePython Exercises/24-DrawAGameBoard.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-size = int(input("What size N of a N x N board would you like me to draw? \n"))
-print()
-
-for x in range(0, size):
-    for dashes in range(0, size):
+def drawGameBoard(size):
+    
+    for x in range(0, size):
+        for dashes in range(0, size):
+            print("  ---  ", end = "")
+        print()
+        for columns in range(0, size):
+            print("|      ", end = "")
+        print("|")
+    for dashes in range(0,size):
         print("  ---  ", end = "")
-    print()
-    for columns in range(0, size):
-        print("|      ", end = "")
-    print("|")
-for dashes in range(0,size):
-    print("  ---  ", end = "")
