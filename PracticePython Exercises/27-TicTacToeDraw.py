8b3511d36d1ca36be385dd91a7f409c215f97146
davidkim827 <davidkim827@gmail.com>
2018-07-13 17:45:08 -0400

    Fixed user input format

--- PracticePython Exercises/27-TicTacToeDraw.py
+++ PracticePython Exercises/27-TicTacToeDraw.py
@@ -12,18 +12,23 @@ def askMove(player):
         while boolean == False:    #continues to loop back to player1's turn if they hit an error in their input
             
             try:
-                moveXrow = int(input("What is your move player 1? (row ENTER col ENTER) \nNumbers 1-3 accepted. \n"))
-                moveXcol = int(input())
+                moveX = input("What is your move player 1? (row,col) \nNumbers 1-3 accepted. \n")
+                moveX = moveX.strip().split(',')
+                moveXrow = int(moveX[0])
+                moveXcol = int(moveX[1])
                 
                 while game[moveXrow-1][moveXcol-1] != 0:    #checks to see if spot has already been filled
                     print("Not valid. There is already something there.")
-                    moveXrow = int(input("What is your move player 1? (row ENTER col ENTER) \nNumbers 1-3 accepted. \n"))
-                    moveXcol = int(input())
-
+                    moveX = input("What is your move player 1? (row,col) \nNumbers 1-3 accepted. \n")
+                    moveX = moveX.strip().split(',')
+                    moveXrow = int(moveX[0])
+                    moveXcol = int(moveX[1])
                 while moveXcol < 1 or moveXcol > 3 or moveXrow < 1 or moveXrow > 3:     #checks to see if the user input is valid for square locations
                     print("Not valid. Please try again.")
-                    moveXrow = int(input("What is your move player 1? (row ENTER col ENTER) \nNumbers 1-3 accepted. \n"))
-                    moveXcol = int(input())
+                    moveX = input("What is your move player 1? (row,col) \nNumbers 1-3 accepted. \n")
+                    moveX = moveX.strip().split(',')
+                    moveXrow = int(moveX[0])
+                    moveXcol = int(moveX[1])
                 game[moveXrow-1][moveXcol-1] = 'X'      #marks location with player1's symbol
                 boolean = True                          #allows for the loop to end
             except:                                     #any other entries besides numbers will result in a recycle of the loop
@@ -33,18 +38,23 @@ def askMove(player):
         while boolean == False:                         #continues to loop back to player1's turn if they hit an error in their input
             
             try:
-                moveOrow = int(input("What is your move player 2? (row ENTER col ENTER) \nNumbers 1-3 accepted.\n"))
-                moveOcol = int(input())
+                moveO = input("What is your move player 2? (row,col) \nNumbers 1-3 accepted. \n")
+                moveO= moveO.strip().split(',')
+                moveOrow = int(moveO[0])
+                moveOcol = int(moveO[1])
                 
                 while game[moveOrow-1][moveOcol-1] != 0:    #checks to see if spot has already been filled
                     print("Not valid. There is already something there.")
-                    moveOrow = int(input("What is your move player 2? (row ENTER col ENTER) \nNumbers 1-3 accepted. \n"))
-                    moveOcol = int(input())
-
+                    moveO = input("What is your move player 2? (row,col) \nNumbers 1-3 accepted. \n")
+                    moveO= moveO.strip().split(',')
+                    moveOrow = int(moveO[0])
+                    moveOcol = int(moveO[1])
                 while moveOcol < 1 or moveOcol > 3 or moveOrow < 1 or moveOrow > 3:     #checks to see if the user input is valid for square location
                     print("Not valid. Please try again.")
-                    moveOrow = int(input("What is your move player 2? (row ENTER col ENTER) \nNumbers 1-3 accepted. \n"))
-                    moveOcol = int(input())
+                    moveO = input("What is your move player 2? (row,col) \nNumbers 1-3 accepted. \n")
+                    moveO= moveO.strip().split(',')
+                    moveOrow = int(moveO[0])
+                    moveOcol = int(moveO[1])
                     
                 game[moveOrow-1][moveOcol-1] = 'O'
                 boolean = True
